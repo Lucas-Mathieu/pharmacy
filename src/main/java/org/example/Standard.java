490a8b32876151c8db8aa78ad410cb4a81470d28
jadmin <jossbessi460@gmail.com>
2025-02-06 00:19:46 +0100

    adding the Standard subclass

--- src/main/java/org/example/Standard.java
+++ src/main/java/org/example/Standard.java
@@ -1,19 +1,15 @@
+package org.example;
+
 import java.util.List;
 
 public class Standard extends Order{
 
-    @Override
-    public List<Product> getOrder() {
-        return orderList;
-    }
-
-    @Override
-    public void removeProduct(Product product) {
-        orderList.remove(product);
+    public Standard(Pharmacy p) {
+        super(p);
     }
 
     @Override
     public boolean isUrgent() {
-        return true;
+        return false;
     }
 }
