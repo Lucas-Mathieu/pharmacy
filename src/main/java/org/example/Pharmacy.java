6e9dd3a62f3c33ea1cb30b2acfc475bead825185
Lucas <lucas.mathieu@edu.esiee-it.fr>
2025-02-05 14:24:24 +0100

    Added confirmation message before deletion

--- src/main/java/org/example/Pharmacy.java
+++ src/main/java/org/example/Pharmacy.java
@@ -3,7 +3,7 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Comparator;
-
+import java.util.Scanner;
 
 public class Pharmacy implements Stockable {
     private List<Product> productList;
@@ -21,7 +21,24 @@ public class Pharmacy implements Stockable {
 
     @Override
     public void removeProduct(String productName) {
-        productList.removeIf(p -> p.getName().equalsIgnoreCase(productName));
+        Scanner scanner = new Scanner(System.in);
+
+        for (Product p : productList) {
+            if (p.getName().equalsIgnoreCase(productName)) {
+                System.out.print("Are you sure you want to remove '" + productName + "' ? (yes/no) : ");
+
+                String response = scanner.nextLine().trim().toLowerCase();
+
+                if (response.equals("yes") || response.equals("y")) {
+                    productList.remove(p);
+                    System.out.println(productName + " has been removed.");
+                } else {
+                    System.out.println("Deletion aborted.");
+                }
+                return;
+            }
+        }
+        System.out.println("Product " + productName + " not found");
     }
 
     public void displayProducts() {
