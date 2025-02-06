f09d1f38eeb9f899f61d27c5163f8a1256b9a0e8
jadmin <jossbessi460@gmail.com>
2025-02-06 15:10:46 +0100

    subclass only used in Auth class

--- src/main/java/org/example/UserConnexion.java
+++ src/main/java/org/example/UserConnexion.java
@@ -1,4 +1,13 @@
 package org.example;
 
-public class UserConnexion {
+public class UserConnexion extends User implements Role {
+    public UserConnexion(String name, String password) {
+        super(name, password);
+
+    }
+
+    @Override
+    public String getRoleName() {
+        return "";
+    }
 }
