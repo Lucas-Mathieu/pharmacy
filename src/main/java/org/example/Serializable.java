052317e65e48351d365e2c38bbb6836ffb511e07
Lucas <lucas.mathieu@edu.esiee-it.fr>
2025-02-06 19:21:06 +0100

    updated Serializable interface

--- src/main/java/org/example/Serializable.java
+++ src/main/java/org/example/Serializable.java
@@ -3,8 +3,10 @@ package org.example;
 import java.util.Map;
 
 public interface Serializable {
-    static void savePharmacy(Pharmacy pharmacy) {};
-    static Pharmacy loadPharmacy() {return null;};
-    static void saveOrders(Map<Product, Integer> orders) {};
-    static Map<Product, Integer> loadOrders() {return null;};
+    static void savePharmacy(Pharmacy pharmacy) {}
+    static Pharmacy loadPharmacy() {return null;}
+    static void saveOrders(Map<Product, Integer> orders) {}
+    static Map<Product, Integer> loadOrders() {return null;}
+    private static void loadUsers() {}
+    private static void saveUsers() {}
 }
