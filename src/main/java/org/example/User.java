18a9f65f0d22fafd2491a6157806a49917e03ae9
jadmin <jossbessi460@gmail.com>
2025-02-06 15:14:09 +0100

    adding the abstract class User for implementation subclass

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,4 +1,28 @@
 package org.example;
 
-public class User {
+public abstract class User {
+    private final String name;
+    private final String password;
+    public User(String name,String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword(){
+
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        String stackedClass = stackTrace[2].getClassName();
+        if (!stackedClass.equals("org.example.Auth")){
+            System.out.println("you dont have acces to this method");
+            return null;
+        }
+        return password;
+    }
+
 }
+
+
