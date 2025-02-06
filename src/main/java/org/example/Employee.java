87dd4197349c75ff24526cb45aa2b7958bad53a1
jadmin <jossbessi460@gmail.com>
2025-02-06 15:16:20 +0100

    adding employee subclass for creating employee session

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,4 +1,20 @@
 package org.example;
 
-public class Employee {
+public class Employee extends User implements Role {
+
+    public Employee(String name, String password) {
+        super(name, password);
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getRoleName() {
+        return "Employee";
+    }
+
+
 }
