a99a048bb0b01580811a68dfcc49ad0c9eb15bf1
jadmin <jossbessi460@gmail.com>
2025-02-07 02:55:19 +0100

    fixing a return returning empty instead of the role

--- src/main/java/org/example/UserConnexion.java
+++ src/main/java/org/example/UserConnexion.java
@@ -1,13 +1,14 @@
 package org.example;
 
 public class UserConnexion extends User implements Role {
-    public UserConnexion(String name, String password) {
+    private final String role;
+    public UserConnexion(String name, String password,String role) {
         super(name, password);
-
+        this.role = role;
     }
 
     @Override
     public String getRoleName() {
-        return "";
+        return role;
     }
 }
